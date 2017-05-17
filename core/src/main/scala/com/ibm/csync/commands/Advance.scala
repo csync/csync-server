6351444975d0c5dccfc6ed709cc5d81f1d787130
Thomas G. Boop <thomasboop@Thomass-MBP-3.lan>
2017-05-17 10:59:27 -0500

    Fixed some bugs related to lvts and rvts

--- core/src/main/scala/com/ibm/csync/commands/Advance.scala
+++ core/src/main/scala/com/ibm/csync/commands/Advance.scala
@@ -22,57 +22,67 @@ import com.ibm.csync.database._
 import com.ibm.csync.session.Session
 import com.ibm.csync.types.Pattern
 
-case class Advance(lvts: Long = Long.MaxValue, rvts: Long = Long.MaxValue, pattern: Seq[String]) extends Command {
+//scalastyle:off
+case class Advance(lvts: Long = Long.MaxValue, rvts: Long = Long.MaxValue, var backwardLimit: Int = Int.MaxValue, var forwardLimit: Int = Int.MaxValue, pattern: Seq[String]) extends Command {
 
   override def doit(us: Session): AdvanceResponse = {
 
     us.transaction { sqlConnection =>
-      //Limit is double actual limit.
+
       val limit = 10
+      backwardLimit = math.min(backwardLimit,limit)
+      forwardLimit = math.min(forwardLimit,limit)
+
       val (patternWhere, patternVals) = Pattern(pattern).asWhere
       val acls = getAcls(sqlConnection, us.userInfo)
       val aclWhere = List.fill(acls.length)("?").mkString(",")
-      val queryVals1 = Seq(rvts) ++ patternVals ++ acls ++ Seq(us.userInfo.userId, limit) ++ Seq(lvts) ++ patternVals ++ acls ++ Seq(us.userInfo.userId, limit)
-      println("queryvals1: " + queryVals1.toString())
-      var minVts: Long = 0
 
+      var minVts: Long = 0
       var maxVts = SqlStatement.queryResult(
         sqlConnection,
         "SELECT last_value FROM latest_vts_seq",
         Seq()
       ) { rs => rs.getLong("last_value") }.head
 
-      val rs1: Seq[Long] = getFromLatest(sqlConnection, patternWhere, aclWhere, queryVals1)
-      if (rs1.length == limit) {
-        maxVts = rs1.head
-      }
-      if (rs1.nonEmpty) {
-        minVts = rs1.last
-      }
+      var forwardList: Seq[Long] = Seq.empty[Long]
 
-
-      val queryVals2 = Seq(rvts, maxVts) ++ patternVals ++ acls ++ Seq(us.userInfo.userId, limit) ++ Seq(lvts, minVts) ++ patternVals ++ acls ++ Seq(us.userInfo.userId, limit)
-      println("queryvals2: " + queryVals2.toString())
-      val rs2: Seq[Long] = getFromAttic(sqlConnection, patternWhere, aclWhere, queryVals2)
-      if (rs2.length == limit) {
-        println("rs2 >= limit rs1: " + rs1.toString() + " rs2: " + rs2.toString())
-        maxVts = rs2.head
-        minVts = rs2.last
-        val filteredRs = rs1.filter(_ < maxVts).filter(_ > minVts)
-        AdvanceResponse(filteredRs ++ rs2, minVts, maxVts)
-
+      if (forwardLimit > 0) {
+        val queryVals1 = Seq(rvts) ++ patternVals ++ acls ++ Seq(us.userInfo.userId, forwardLimit) ++ Seq(rvts) ++ patternVals ++ acls ++ Seq(us.userInfo.userId, forwardLimit, forwardLimit)
+        println(queryVals1.toString())
+        forwardList = getNewData(sqlConnection, patternWhere, aclWhere, queryVals1)
+        if (forwardList.length == forwardLimit) { //We asked for backwards limit number of items and we received that many, new lvts is the lowest vts of that list
+          maxVts = forwardList.last
+        }
       } else {
-
-        println("rs2 < limit rs1: " + rs1.toString() + " rs2: " + rs2.toString())
-        if (minVts > lvts) {
-          minVts = lvts
-        }
-        if (rvts != Long.MaxValue && maxVts < rvts) {
+        if(rvts != Long.MaxValue) {
           maxVts = rvts
+        } else if (lvts != Long.MaxValue) {
+          maxVts = lvts
         }
-        AdvanceResponse(rs1 ++ rs2, minVts, maxVts)
       }
+
+      var backwardList: Seq[Long] = Seq.empty[Long]
+      if (backwardLimit > 0) {
+        val queryVals2 = Seq(lvts) ++ patternVals ++ acls ++ Seq(us.userInfo.userId, backwardLimit) ++ Seq(lvts) ++ patternVals ++ acls ++ Seq(us.userInfo.userId, backwardLimit, backwardLimit)
+        println(queryVals2.toString())
+        backwardList = getOldData(sqlConnection, patternWhere, aclWhere, queryVals2)
+        if (backwardList.length == backwardLimit) { //We asked for backwards limit number of items and we received that many, new lvts is the lowest vts of that list
+          minVts = backwardList.last
+        }
+      }
+      else {
+        minVts = math.min(lvts,rvts)
+        if (forwardList.length > 0) {
+          minVts = math.min(minVts, forwardList.head)
+        }
+        else {
+          minVts = math.min(maxVts,minVts)
+        }
+      }
+
+      AdvanceResponse(backwardList ++ forwardList, minVts, maxVts)
     }
+
   }
 
   def addTerm(op: String, term: String): String = {
@@ -83,27 +93,7 @@ case class Advance(lvts: Long = Long.MaxValue, rvts: Long = Long.MaxValue, patte
     }
   }
 
-  private def getFromAttic(sqlConnection: Connection, patternWhere: String, aclWhere: String, queryVals2: Seq[Any]) =
-    SqlStatement.queryResult(
-      sqlConnection,
-      s"""
-          (SELECT vts FROM attic WHERE (vts > ? AND vts < ?)
-              ${addTerm("AND", patternWhere)}
-					    AND (aclid IN ($aclWhere) OR creatorid = ?)
-					    ORDER BY vts ASC LIMIT ?)
-
-         UNION ALL
-
-         (SELECT vts FROM attic WHERE (vts < ? AND vts > ?)
-                       ${addTerm("AND", patternWhere)}
-         					    AND (aclid IN ($aclWhere) OR creatorid = ?)
-                      ORDER BY vts DESC LIMIT ?)
-          ORDER BY vts DESC
-          """,
-      queryVals2
-    ) { rs => rs.getLong("vts") }
-
-  private def getFromLatest(sqlConnection: Connection, patternWhere: String, aclWhere: String, queryVals1: Seq[Any]) =
+  private def getNewData(sqlConnection: Connection, patternWhere: String, aclWhere: String, queryVals1: Seq[Any]) =
     SqlStatement.queryResult(
       sqlConnection,
       s"""
@@ -112,15 +102,36 @@ case class Advance(lvts: Long = Long.MaxValue, rvts: Long = Long.MaxValue, patte
 					    AND (aclid IN ($aclWhere) OR creatorid = ?)
 					    ORDER BY vts ASC LIMIT ?)
           UNION ALL
-         ( SELECT vts FROM latest WHERE vts < ?
+         ( SELECT vts FROM attic WHERE vts > ?
                        ${addTerm("AND", patternWhere)}
          					    AND (aclid IN ($aclWhere) OR creatorid = ?)
 
-         					    ORDER BY vts DESC LIMIT ?)
-                  ORDER BY vts DESC
+         					    ORDER BY vts ASC LIMIT ?)
+                  ORDER BY vts ASC LIMIT ?
           """,
       queryVals1
 
     ) { rs => rs.getLong("vts") }
+
+  private def getOldData(sqlConnection: Connection, patternWhere: String, aclWhere: String, queryVals1: Seq[Any]) =
+    SqlStatement.queryResult(
+      sqlConnection,
+      s"""
+          (SELECT vts FROM latest WHERE vts < ?
+              ${addTerm("AND", patternWhere)}
+					    AND (aclid IN ($aclWhere) OR creatorid = ?)
+					    ORDER BY vts DESC LIMIT ?)
+          UNION ALL
+         ( SELECT vts FROM attic WHERE vts < ?
+                       ${addTerm("AND", patternWhere)}
+         					    AND (aclid IN ($aclWhere) OR creatorid = ?)
+
+         					    ORDER BY vts DESC LIMIT ?)
+                  ORDER BY vts DESC LIMIT ?
+          """,
+      queryVals1
+
+    ) { rs => rs.getLong("vts") }
+
 
 }
--- core/src/test/scala/com/ibm/csync/commands/AdvanceTests.scala
+++ core/src/test/scala/com/ibm/csync/commands/AdvanceTests.scala
@@ -41,7 +41,7 @@ class AdvanceTests extends FunSuite with Matchers {
       Token("demoToken"), SessionId())(f)
   }
 
-  test("Test Simple Advance") {
+  test("Test Simple Advance with no specified LVTS/RVTS") {
     val session = fakeSession { _ => Future.successful(()) }
     try {
       Pub(99, Seq("a"), Some("x"), false, None, None).doit(session)
@@ -50,10 +50,10 @@ class AdvanceTests extends FunSuite with Matchers {
       Pub(102, Seq("a"), Some("yyy"), false, None, None).doit(session)
       val lastVTS = Pub(103, Seq("a"), Some("yyyy"), false, None, None).doit(session).vts
       val advanceResponse = Advance(pattern = Seq("a")).doit(session)
-      println("advanceResponse: " + advanceResponse.toString)
       advanceResponse.rvts should be(lastVTS)
       advanceResponse.vts.head should be(lastVTS)
       advanceResponse.vts.size should be(1)
+      println(advanceResponse.toString())
     } finally {
       session.close()
     }
@@ -67,13 +67,13 @@ class AdvanceTests extends FunSuite with Matchers {
       Pub(101, Seq("a"), Some("yy"), false, None, None).doit(session)
       val lastAVTS = Pub(102, Seq("a"), Some("yyy"), false, None, None).doit(session).vts
       val maxVTS = Pub(103, Seq("b"), Some("yyyy"), false, None, None).doit(session).vts
-      val advanceResponse = Advance(1,1, Seq("a")).doit(session)
-      println("advanceResponse: " + advanceResponse.toString())
-      println(Fetch(List(5,6)).doit(session).toString)
-
+      val advanceResponse = Advance(1,1,50,50, Seq("a")).doit(session)
+      println(advanceResponse.toString())
       advanceResponse.rvts should be(maxVTS)
+      advanceResponse.lvts should be (0)
       advanceResponse.vts.last should be(lastAVTS)
       advanceResponse.vts.size should be(1)
+
     } finally {
       session.close()
     }
@@ -93,13 +93,14 @@ class AdvanceTests extends FunSuite with Matchers {
       Pub(107, Seq("j"), Some("yyyyyyyy"), false, None, None).doit(session)
       val tenthPubVTS = Pub(108, Seq("k"), Some("yyyyyyyyy"), false, None, None).doit(session).vts
       Pub(109, Seq("l"), Some("yyyyyyyyyy"), false, None, None).doit(session)
-      val advanceResponse = Advance(1,1, Seq("*")).doit(session)
+      val advanceResponse = Advance(1,1,50,50,Seq("*")).doit(session)
+      println(advanceResponse.toString())
       //vts starts at 1, so 10 entries should get us to 11
-      println("advanceResponse: " + advanceResponse.toString())
-      println(Fetch(Seq(3)).doit(session).response.toString())
       advanceResponse.rvts should be(tenthPubVTS)
       //Max advance return is 10
       advanceResponse.vts.size should be(10)
+      advanceResponse.lvts should be(0)
+      advanceResponse.rvts should be(tenthPubVTS)
     } finally {
       session.close()
     }
@@ -130,6 +131,7 @@ test("Test Advance that reaches limit for both lvts and rvts") {
       Pub(118, Seq("l"), Some("yyyyyyyyyy"), false, None, None).doit(session)
       val lastPubVTS = Pub(119, Seq("l"), Some("yyyyyyyyyy"), true, None, None).doit(session)
       val advanceResponse = Advance(pattern = Seq("*")).doit(session)
+      println(advanceResponse.toString())
       //vts starts at 1, so 10 entries should get us to 11
       println("advanceResponse: " + advanceResponse.toString())
       advanceResponse.rvts should be(lastPubVTS.vts)
@@ -155,17 +157,7 @@ test("Test Advance that reaches limit for both lvts and rvts") {
       Pub(105, Seq("g"), Some("yyyyyy"), false, None, None).doit(session)
       Pub(106, Seq("h"), Some("yyyyyyy"), false, None, None).doit(session)
       Pub(107, Seq("j"), Some("yyyyyyyy"), false, None, None).doit(session)
-      Pub(1, Seq("k"), Some("yyyyyyyyy"), false, None, None).doit(session)
-//      Pub(108, Seq("a"), Some("x"), true, None, None).doit(session)
-//      Pub(109, Seq("b"), Some("y"), true, None, None).doit(session)
-//      Pub(110, Seq("c"), Some("yy"), true, None, None).doit(session)
-//      Pub(111, Seq("d"), Some("yyy"), true, None, None).doit(session)
-//      Pub(112, Seq("e"), Some("yyyy"), true, None, None).doit(session)
-//      Pub(113, Seq("f"), Some("yyyyy"), true, None, None).doit(session)
-//      Pub(114, Seq("g"), Some("yyyyyy"), true, None, None).doit(session)
-//      Pub(115, Seq("h"), Some("yyyyyyy"), true, None, None).doit(session)
-//      Pub(116, Seq("j"), Some("yyyyyyyy"), true, None, None).doit(session)
-//      Pub(117, Seq("k"), Some("yyyyyyyyy"), true, None, None).doit(session)
+      Pub(108, Seq("k"), Some("yyyyyyyyy"), false, None, None).doit(session)
       Pub(120, Seq("a"), Some("yx"), false, Option("$publicWrite"), None).doit(session)
       Pub(121, Seq("b"), Some("yx"), false, Option("$publicWrite"), None).doit(session)
       Pub(122, Seq("c"), Some("yxy"), false, Option("$publicWrite"), None).doit(session)
@@ -177,17 +169,37 @@ test("Test Advance that reaches limit for both lvts and rvts") {
       Pub(128, Seq("j"), Some("yxyyyyyyy"), false, Option("$publicWrite"), None).doit(session)
       val lastPubVTS = Pub(129, Seq("k"), Some("yxyyyyyyyy"), false, Option("$publicWrite"), None).doit(session)
       val advanceResponse = Advance(pattern = Seq("*")).doit(session)
-      //vts starts at 1, so 10 entries should get us to 11
-      println("advanceResponse: " + advanceResponse.toString())
+      println(advanceResponse.toString())
+      //Should return lvts of 13 and rvts of 31 with 19 entries
       advanceResponse.rvts should be(lastPubVTS.vts)
-      //Max advance return is 10
-      advanceResponse.vts.size should be(19)
+      advanceResponse.vts.size should be(advanceResponse.rvts-advanceResponse.lvts+1)
+
       val advanceResponse2 = Advance(advanceResponse.lvts, advanceResponse.rvts, pattern = Seq("*")).doit(session)
-      println("advanceResponse2: " + advanceResponse2.toString())
+      println(advanceResponse2.toString())
+      advanceResponse2.vts.size should be (10)
     } finally {
       session.close()
     }
   }
+
+  test("Test only go forwards") {
+    val session = fakeSession { _ => Future.successful(()) }
+    try {
+      Pub(99, Seq("a"), Some("x"), false, None, None).doit(session)
+      Pub(100, Seq("a"), Some("y"), false, None, None).doit(session)
+      Pub(101, Seq("a"), Some("yy"), false, None, None).doit(session)
+      Pub(102, Seq("a"), Some("yyy"), false, None, None).doit(session)
+      val lastVTS = Pub(103, Seq("a"), Some("yyyy"), false, None, None).doit(session).vts
+      val advanceResponse = Advance(backwardLimit = 0,pattern = Seq("a")).doit(session)
+      println(advanceResponse.toString())
+      advanceResponse.rvts should be(lastVTS)
+      advanceResponse.vts.size should be(0)
+      advanceResponse.lvts should be(lastVTS)
+    } finally {
+      session.close()
+    }
+  }
+
   //TODO Add advance tests for changed ACLs, updates and deletes
   // scalastyle:on magic.number
 }
